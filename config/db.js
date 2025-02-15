ffde7b98fe80b238f67005033b8c520df8f7fdd6
Taylor Martin <taylormartind@gmail.com>
2025-02-15 18:22:08 -0500

    made changes to env variables

--- config/db.js
+++ config/db.js
@@ -1,18 +1,10 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const dbName = process.env.DB_NAME;
-const dbUser = process.env.DB_USER;
-const dbPass = process.env.DB_PASSWORD;
+const dbURL = process.env.DB_URL;
 
 const connectDB = async () => {
     try {
-        console.log('Attempting to connect to DB');
-
-        await mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@cluster0.vb98d.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-
+        await mongoose.connect(`${dbURL}`, {});
         console.log('MongoDB connected');
     } catch (error) {
         console.error('Error connecting to MongoDB: ', error.message);
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { buildSchema } = require('graphsql');
-const { graphqlHTTP } = require('/');
+const connectDB = require('./config/db');
 const app = express();
 const SERVER_PORT = 4000;
 
 app.listen(SERVER_PORT, () => {
     console.log('Sever started')
-    console.log('http://localhost:4000/graphql')
+    // db connection
+    connectDB();
+    console.log('http://localhost:4000/')
 });
-
