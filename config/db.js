2d08a849472bc0687d4937891b40453d94d074fa
Taylor Martin <taylormartind@gmail.com>
2025-02-15 17:12:54 -0500

    created environment variables for db

--- config/db.js
+++ config/db.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASSWORD;
 
 const connectDB = async () => {
     try {
         console.log('Attempting to connect to DB');
 
-        await mongoose.connect('dbURL', {
+        await mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@cluster0.vb98d.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
