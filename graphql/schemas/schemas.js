e1f61fc99f7046b125f0c43df360f3689e5036fd
Taylor Martin <taylormartind@gmail.com>
2025-02-16 15:23:55 -0500

    created base schema for defining queries and mutations globally

--- graphql/schemas/schemas.js
+++ graphql/schemas/schemas.js
@@ -1,13 +1,18 @@
-const fs = require("fs");
-const path = require("path");
-const { buildSchema } = require("graphql");
+const fs = require('fs');
+const path = require('path');
+const { buildSchema } = require('graphql');
 
-// read graphql files from schemas folder (keeping dynamic)
-const schemaFiles = fs.readdirSync(__dirname)
-    .filter(file => file.endsWith(".graphql"))
-    .map(file => fs.readFileSync(path.join(__dirname, file), "utf8"));
+// read independent schema files
+const userSchema = fs.readFileSync(path.join(__dirname, 'userSchema.graphql'), 'utf8');
+const employeeSchema = fs.readFileSync(path.join(__dirname, 'employeeSchema.graphql'), 'utf8');
 
-// merges all schemas inside any graphql file from folder
-const schema = buildSchema(schemaFiles.join("\n"));
+// base schema to define query/mutation globally (getting a duplication error if i dont do this)
+const baseSchema = `
+    type Query
+    type Mutation
+`;
+
+// merge schemas
+const schema = buildSchema(`${baseSchema}\n${userSchema}\n${employeeSchema}`);
 
 module.exports = schema;
